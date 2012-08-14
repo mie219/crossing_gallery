3e5354d93aa4dc0b2f31b3eaeaa2c9082231ea41
mie219 <mie219.h@gmail.com>
2012-08-14 14:09:31 +0900

    added function generate 24 section tags

--- web/js/ui.js
+++ web/js/ui.js
@@ -2,3 +2,16 @@
  * web/js/ui.js
  * define user interface
  */
+
+$(function () {
+  var html = "";
+  var grid = ['1x1', '1x1', '1x1', '1x1', '1x1', '1x2', '1x2', '2x1', '2x1', '2x2'];
+  var rand = 0;
+
+  for (var i = 0; i < 24; i++) {
+    rand = Math.ceil(Math.random() * 10);
+    html += '<section class="grid' + grid[rand] + '"></section>';
+  }
+  html += '<div style="clear: both;"></div>';
+  $("#img-grid").prepend(html);
+});

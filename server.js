37f294ff3ecf4441eafe678200d06b9160e7abaa
ww24 <ww24gm@gmail.com>
2012-08-14 16:27:18 +0900

    modefied package.json to use http-proxy modefied server.js for http-proxy

--- server.js
+++ server.js
@@ -3,6 +3,25 @@
  * server side application
  */
 
-var nws = require("node-web-server");
+var proxy = require("http-proxy"),
+    nws = require("node-web-server"),
+    CrossingGallery = require("lib/crossing_gallery.js");
+
+// proxy settings
+proxy.createServer({
+  router: {
+    ".+/api": "localhost:8080",
+    ".+": "localhost:8080"
+  }
+}).listen(80);
 
 nws.run("config/http.conf");
+
+// API
+/*
+var cg = new CrossingGallery();
+cg.require("views");
+cg.require("modules");
+cg.require("proxies");
+cg.run();
+*/

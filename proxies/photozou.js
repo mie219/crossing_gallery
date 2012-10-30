c4ab5e35f39480a14d1467ac48a7ba9e4480fea3
ww24 <ww24gm@gmail.com>
2012-10-30 23:46:37 +0900

    added proxy for Photozou API

--- proxies/photozou.js
+++ proxies/photozou.js
@@ -1,1 +1,44 @@
-/* フォト蔵 */
+/*
+ * apis/photozou.js
+ * photozou API wrapper
+ */
+
+var Request = require("../lib/request"),
+    xml_parse = require("../lib/xml_parse");
+
+function API() {
+  var req = new Request("api.photozou.jp");
+  
+  this.search = function (options, callback) {
+    var path = "/rest/search_public",
+        queries = options || {};
+    
+    if (typeof options !== "object")
+      callback = options;
+    
+    req.get({
+      path: path,
+      queries: queries
+    }, function (res) {
+      var photos = xml_parse(res, "info > photo");
+      
+      // format photos info
+      photos = photos.map(function (photo) {
+        return {
+          title   : photo.photo_title,
+          url     : photo.image_url,
+          original: photo.url,
+          site    : "フォト蔵"
+        };
+      });
+      
+      callback(photos);
+    });
+  };
+  
+  this.public = function () {
+    
+  };
+}
+
+module.exports = new API();

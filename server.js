4f89bfd239a1833521b6b99bb2904855879659c9
ww24 <ww24gm@gmail.com>
2012-10-30 23:42:32 +0900

    fixed server.js

--- server.js
+++ server.js
@@ -3,25 +3,35 @@
  * server side application
  */
 
-var proxy = require("http-proxy"),
+// settings
+var proxy_port = 8888,
+    api_port = 8000,
+    api_prefix = "/api/";
+
+var fs = require("fs"),
+    proxy = require("http-proxy"),
     nws = require("node-web-server"),
-    CrossingGallery = require("lib/crossing_gallery.js");
+    CrossingGallery = require("./lib/crossing_gallery.js");
+
+// create logs dir
+if (! fs.existsSync(__dirname + "/logs"))
+  fs.mkdirSync("logs");
+
+// route settings
+var router = {};
+router["[0-9a-zA-Z-.]+" + api_prefix] = "localhost:" + api_port;
+router[".+"] = "localhost:8080";
 
 // proxy settings
 proxy.createServer({
-  router: {
-    ".+/api": "localhost:8080",
-    ".+": "localhost:8080"
-  }
-}).listen(80);
+  router: router
+}).listen(proxy_port);
 
+// static server
 nws.run("config/http.conf");
 
-// API
-/*
+// Crossing Gallery server
 var cg = new CrossingGallery();
-cg.require("views");
-cg.require("modules");
-cg.require("proxies");
-cg.run();
-*/
+cg.listen(api_port, "localhost", api_prefix);
+
+console.log("Proxy server running at http://127.0.0.1:" + proxy_port + "/");
